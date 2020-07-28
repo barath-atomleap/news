13704e0217a807b6f9ca7336b408949d01083e59
leonardofj <leonardofj@gmail.com>
2020-07-28 15:17:33 +0200

    adding requirements to pipenv

--- src/news_processing.py
+++ src/news_processing.py
@@ -4,7 +4,7 @@ import re
 
 
 def news_boilerplater(html: str):
-    """
+  """
   Receives a web page from a news source with `html` contents.
   Returns the title, text content and publication date of this news entry.
   Args:
@@ -12,31 +12,30 @@ def news_boilerplater(html: str):
   Returns:
       title (str), content (str), publication_date (str)
   """
-
-    def preprocess_text(text: str):
-        """
+  def preprocess_text(text: str):
+    """
     Cleaning and processing text extracted from web page with trafilatura.
     :param text: input string with text
     :return: clean text
     """
 
-        text = text.strip()
-        try:
-            text = clean(text)
-        except Exception as e:
-            print("{}: Can't preprocess news article text with cleantext package. Keeping the original text.", str(e))
-            text = text
-        try:
-            text = text.encode('latin-1').decode('unicode_escape')
-        except Exception as e:
-            print("{}: Can't perform encoding and decoding on news article text. Keeping the original text.", str(e))
-            text = text
-        text = re.sub("\s+", " ", text)
-        return text.strip()
+    text = text.strip()
+    try:
+      text = clean(text)
+    except Exception as e:
+      print("{}: Can't preprocess news article text with cleantext package. Keeping the original text.", str(e))
+      text = text
+    try:
+      text = text.encode('latin-1').decode('unicode_escape')
+    except Exception as e:
+      print("{}: Can't perform encoding and decoding on news article text. Keeping the original text.", str(e))
+      text = text
+    text = re.sub("\s+", " ", text)
+    return text.strip()
 
-    page_content = trafilatura.extract(html)
-    page_metadata = trafilatura.metadata.extract_metadata(html)
-    article_publication_date = page_metadata.date
-    article_title = preprocess_text(str(page_metadata.title))
-    page_content = preprocess_text(str(page_content))
-    return article_title, page_content, article_publication_date
+  page_content = trafilatura.extract(html)
+  page_metadata = trafilatura.metadata.extract_metadata(html)
+  article_publication_date = page_metadata.date
+  article_title = preprocess_text(str(page_metadata.title))
+  page_content = preprocess_text(str(page_content))
+  return article_title, page_content, article_publication_date
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -7,51 +7,51 @@ from googletrans import Translator
 
 
 def clean_url(url, keep_www=False):
-    url = url.strip()
-    url = url.replace('https://', '').replace('http://', '').rstrip('/')
-    if not keep_www:
-        url = url.replace('www.', '')
-    split_url = url.split('/')
-    split_url[0] = split_url[0].lower()
-    return '/'.join(split_url)
+  url = url.strip()
+  url = url.replace('https://', '').replace('http://', '').rstrip('/')
+  if not keep_www:
+    url = url.replace('www.', '')
+  split_url = url.split('/')
+  split_url[0] = split_url[0].lower()
+  return '/'.join(split_url)
 
 
 def save_blob(url, text):
-    try:
-        blob_storage = get_config('blob_storage')
-        # Create the BlobServiceClient object which will be used to create a container client
-        blob_service_client = BlobServiceClient.from_connection_string(blob_storage['connection_string'])
+  try:
+    blob_storage = get_config('blob_storage')
+    # Create the BlobServiceClient object which will be used to create a container client
+    blob_service_client = BlobServiceClient.from_connection_string(blob_storage['connection_string'])
 
-        # Create a blob client using the local file name as the name for the blob
-        blob_client = blob_service_client.get_blob_client(container=blob_storage['container'], blob=url)
+    # Create a blob client using the local file name as the name for the blob
+    blob_client = blob_service_client.get_blob_client(container=blob_storage['container'], blob=url)
 
-        print("\nUploading to Azure Storage as blob:\n\t" + url)
+    print("\nUploading to Azure Storage as blob:\n\t" + url)
 
-        # Upload the created file
-        blob_client.upload_blob(text)
-    except Exception as e:
-        logging.error('Exception:', e)
-    return url
+    # Upload the created file
+    blob_client.upload_blob(text)
+  except Exception as e:
+    logging.error('Exception:', e)
+  return url
 
 
 def is_text_in_english(text: str):
-    """
+  """
     Detects the language of a given text and determines whether it is in English or not.
     :return: bool for whether text is in English or not
     """
-    language = cld3.get_language(text)
-    if str(language[0]) == "en":
-        return True
-    else:
-        return False
+  language = cld3.get_language(text)
+  if str(language[0]) == "en":
+    return True
+  else:
+    return False
 
 
 def translate_to_english(text: str):
-    """
+  """
     Translates given text to English.
     :param text: article text (str)
     :return: translated text (str)
     """
-    translator = Translator()
-    translated_text = translator.translate(text, dest="en")
-    return translated_text.text
+  translator = Translator()
+  translated_text = translator.translate(text, dest="en")
+  return translated_text.text
