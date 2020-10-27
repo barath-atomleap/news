039da90fb9263cb0b88ac2757375db3750e37d68
konstantina-atomleap <konstantina@atomleap.com>
2020-10-27 13:10:49 +0100

    feat: azure translation and lang detection services

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,5 @@
 import logging
-
+import requests
 import cld3
 from azure.storage.blob import BlobServiceClient
 from delphai_backend_utils.config import get_config
@@ -29,8 +29,12 @@ def is_text_in_english(text: str):
     Detects the language of a given text and determines whether it is in English or not.
     :return: bool for whether text is in English or not
     """
-  language = cld3.get_language(text)
-  if str(language[0]) == "en":
+  # language = cld3.get_language(text)
+  # if str(language[0]) == "en":
+
+  response = requests.post("https://api.delphai.blue/translation/delphai.Translation.detect_language",
+                           json={"text": text}).json()
+  if response["language"] == "en":
     return True
   else:
     return False
@@ -42,6 +46,9 @@ def translate_to_english(text: str):
     :param text: article text (str)
     :return: translated text (str)
     """
-  translator = Translator()
-  translated_text = translator.translate(text, dest="en")
-  return translated_text.text
+  # translator = Translator()
+  # translated_text = translator.translate(text, dest="en")
+  response = requests.post("https://api.delphai.blue/translation/delphai.Translation.translate",
+                           json={"text": text}).json()
+  # return translated_text.text
+  return response["translation"]
